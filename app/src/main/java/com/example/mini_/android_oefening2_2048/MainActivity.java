448333bbf8ca65e1e543f3b45d098c528f4f8b6d
MathieuFQT <mathieu.fouquet@gmail.com>
2015-10-13 15:09:01 +0200

    Initial Commit

---
+++ app/src/main/java/com/example/mini_/android_oefening2_2048/MainActivity.java
@@ -0,0 +1,51 @@
+package com.example.mini_.android_oefening2_2048;
+
+import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.View;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+
+    }
+
+    public void btncontinueClicked(View v)
+    {
+
+    }
+    public void btnNewGameClicked(View v)
+    {
+        this.getClass().getSimpleName()
+    }
+    public void  btnAboutClicked(View v)
+    {
+        AlertDialog.Builder aboutBuilder = new AlertDialog.Builder(this);
+        aboutBuilder.setMessage(getString(R.string.aboutPage));
+        aboutBuilder.setCancelable(true);
+
+        AlertDialog about = aboutBuilder.create();
+        about.show();
+    }
+
+    public void btnExitClicked(View v)
+    {
+        this.finish();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu items for use in the action bar
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_activity_actions, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+}
